ed4b1da685e1617d6c43711efa241d32f81e8de3
Benedict Beno <ben.b.hoorkwap@gmail.com>
2019-11-10 18:26:54 +0000

    second commit

--- src/clientSideJs/clientSide.js
+++ src/clientSideJs/clientSide.js
@@ -1,19 +1,28 @@
 
+window.addEventListener("DOMContentLoaded", () => {
 
-const onClick = () => {
-        let el = document.getElementById("graph");
-        const context = el.getContext("2d");          // retrieves the 2d drawing context.
-        context.beginPath();                // begin drawing path command.
-        
-        context.arc(200, 200, 50, 0, 2 * 3.1415)
-        context.closePath();
-        context.lineWidth = 1;
-        context.strokeStyle = "#ae5544";
-        context.fillStyle = "#ae5544";
+      const draw = () => {
+            let el = document.getElementById("graph");
+            const context = el.getContext("2d");          // retrieves the 2d drawing context.
+            context.beginPath();                // begin drawing path command.
+            
+            context.arc(200, 200, 50, 0, 2 * 3.1415)
+            context.closePath();
+            context.lineWidth = 1;
+            context.strokeStyle = "#ae5544";
+            context.fillStyle = "#ae5544";
+    
+            context.stroke();
+            context.fill();
+            
+      }
 
-        context.stroke();
-        context.fill();
-        
-  }
+      // invoke the draw() function
+      let handleId = setTimeout(() => {
+            draw();
+      }, 2000)
+
+})
+
 
 
